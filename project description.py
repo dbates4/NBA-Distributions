05d5a63dac25ea5036f11813a0bb17fcecaa465b
Dylan Bates <dylanbates4@gmail.com>
2020-07-08 17:03:00 -0600

    First commit

---
+++ project description.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 21:40:34 2020
+
+Calculate team distributions
+Calculate player distributions
+
+What distributions matter most?
+Does variance matter?
+Does mean matter?
+
+@author: dylan
+"""
+
---
+++ team distributions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 21:45:14 2020
+
+@author: dylan
+"""
+
+import pandas as pd
+
+datapath = "C:/Users/dylan/Datasets/NBA/box scores 2016 to 2017.csv"
+
+df = pd.read_csv(datapath)
+
+df = df.drop(columns=['Unnamed: 0'])
+
+df_teamlevel = df.groupby(['Team','Date'])
